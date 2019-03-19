4fc913152187ae06c6c9ed198eb8b6ddbf83d7af
Mpho Sello <mphoscallos.mm@gmail.com>
2019-03-19 15:43:38 +0200

    Second commit

--- newpackage/recursion.py
+++ newpackage/recursion.py
@@ -1,9 +1,15 @@
 def sum_array(array):
-    return 0 if array == None else sum(sorted(array)[1:-1] for x in range(len(array or [])-2))
+    def sum_array(array):
 
-    '''Return sum of all items in array'''
+    total =0
+    for i in array:
+        total= total + i
+        '''Return sum of all items in array'''
+    return total
+
+
+
 def fibonacci(n):
-    def rec_fib(n):
     '''inefficient recursive function as defined, returns Fibonacci number'''
     if n > 1:
         return fibonacci(n-1) + fibonacci(n-2)
@@ -13,14 +19,14 @@ def fibonacci(n):
 def factorial(n):
      if n == 1:
         return n
-    else:
+     else:
         return n * factorial(n-1)
 
 
 
     '''Return n!'''
 def reverse(word):
-    words = Sentence.split(" ")
+    words = word.split(" ")
 
     # Reversing each word and creating
     # a new list of words
@@ -31,6 +37,7 @@ def reverse(word):
     # to for a new Sentence
     newSentence = " ".join(newWords)
 
-    return newSentence 
+    return newSentence
+
 
     '''Return word in reverse'''
--- newpackage/sorting.py
+++ newpackage/sorting.py
@@ -1,9 +1,38 @@
 def bubble_sort(items):
+    n = len(arr)
+    for i in range(n-1,0,-1):
+        for j in range(i):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
     '''Return array of items, sorted in ascending order'''
 def merge_sort(items):
+     if len(items) < 2:return items
+
+    result,mid = [],int(len(items)/2)
+
+    y = merge_sort(items[:mid])
+    z = merge_sort(items[mid:])
+
+    while (len(y) > 0) and (len(z) > 0):
+            if y[0] > z[0]:result.append(z.pop(0))
+            else:result.append(y.pop(0))
+
+    result.extend(y+z)
+    return result
 
     '''Return array of items, sorted in ascending order'''
 def quick_sort(items):
+    if len(items) == 0:
+        return items
+    start = items[0]
+    first = [x for x in items if x == start]
+    smaller = quick_sort([x for x in items if x < start])
+    larger = quick_sort([x for x in items if x > start])
+
+    '''Return array o
+    gnbfv dcswf items, sorted in ascending order'''
+    return smaller + first + larger
 
     '''Return array of items, sorted in ascending order'''
