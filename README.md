1748bc5b7316cec4a9a949ec480de62494738de6
Mpho Sello <mphoscallos.mm@gmail.com>
2019-03-19 16:45:46 +0200

    fixing errors in recursion

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 #newpackage
 
 ## building this package locally
-
-## installing this package from github
-'pip install git +https://github.com/mphoscallos/newpackage.git'
-
-## updating this package from github
-'pip install --upgrade git+https://github.com/mphoscallos/newpackage.git'
--- newpackage/recursion.py
+++ newpackage/recursion.py
@@ -1,42 +1,27 @@
 def sum_array(array):
+
     total =0
     for i in array:
         total= total + i
         '''Return sum of all items in array'''
     return total
 
-
-
-
 def fibonacci(n):
-    '''inefficient recursive function as defined, returns Fibonacci number'''
-    if n > 1:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
         return fibonacci(n-1) + fibonacci(n-2)
-    return n
 
-    '''Return nth term in fibonacci sequence'''
+
 def factorial(n):
-     if n == 1:
-        return n
-     else:
-        return n * factorial(n-1)
+    if n < 2:
+        return 1
 
+    else:
+        return n * factorial(n - 1)
 
-
-    '''Return n!'''
 def reverse(word):
-    words = word.split(" ")
-
-    # Reversing each word and creating
-    # a new list of words
-    # List Comprehension Technique
-    newWords = [word[::-1] for word in words]
-
-    # Joining the new list of words
-    # to for a new Sentence
-    newSentence = " ".join(newWords)
-
-    return newSentence
-
-
-    '''Return word in reverse'''
+    rev=word[::-1]
+    return rev
